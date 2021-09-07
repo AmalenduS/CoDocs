6912bf13a5e6f32c6d2864968e07305919ec7be2
AmalenduS <88838636+AmalenduS@users.noreply.github.com>
2021-09-07 19:17:27 +0530

    Delete clean.sh

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -rf organizations/ordererOrganizations
-rm -rf organizations/peerOrganizations
-rm -rf organizations/fabric-ca/patient
-rm -rf organizations/fabric-ca/provider
-rm -rf organizations/fabric-ca/admin
-rm -rf organizations/fabric-ca/ordererOrg
-
