dda27a03bb4a9907e3fa74ed8e120a57ed8a5578
AmalenduS <88838636+AmalenduS@users.noreply.github.com>
2021-09-07 19:31:48 +0530

    Add files via upload

---
+++ ca_network/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf organizations/ordererOrganizations
+rm -rf organizations/peerOrganizations
+rm -rf organizations/fabric-ca/patient
+rm -rf organizations/fabric-ca/provider
+rm -rf organizations/fabric-ca/admin
+rm -rf organizations/fabric-ca/ordererOrg
+
